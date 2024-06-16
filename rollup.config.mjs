f5ba3cb9d2eb73079ce16cbc1856698d1b01cc0d
Oliver Cox <github@konx.dev>
2024-06-16 13:58:45 +0100

    added terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
 
 import packageJson from './package.json' assert { type: 'json' };
 
@@ -20,7 +21,7 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })]
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), terser()]
   },
   {
     input: 'dist/esm/types/index.d.ts',
